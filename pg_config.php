352a0f4f01edde0336267e05f3e08eb993c1d191
mayngutsy <moschis360@gmail.com>
2019-12-08 16:40:41 +0100

    all

---
+++ pg_config.php
@@ -0,0 +1,9 @@
+<?php
+// Config file
+if(!defined("ASBPATH") )
+	define("ASBPATH", dirname(__FILE__). '/' );
+
+/** Sets up PayGate vars and included files. */
+require_once(ABSPATH . 'pg_setting.php');
+
+?>
---
+++ pg_setting.php
@@ -0,0 +1,11 @@
+<?php 
+//main setup file
+define('PGINC', 'include');
+
+// Include files required for initialization.
+require( ABSPATH . PGINC . '/load.php' );
+require( ABSPATH . PGINC . '/default_constants.php' );
+require_once( ABSPATH . WPINC . '/plugins.php' );
+
+require_once( ABSPATH . WPINC . '/functions.php' );
+?>
